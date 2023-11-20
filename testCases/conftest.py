80975ec449ffab049e72fc14fe4879dd5c3b5601
MdSikder <mdporagbsmrstu@gmail.com>
2023-11-20 22:44:32 +0600

    few updates to run test cases by preferred browser/cross browser/parallel

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,8 +1,3 @@
-import time
-
-from selenium import webdriver
-import pytest
-from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -14,47 +9,30 @@ from webdriver_manager.microsoft import IEDriverManager
 
 
 @pytest.fixture()
-def setup():
-    # driver = webdriver.Chrome(ChromeDriverManager().install())
-    # driver = webdriver.Ie(service=IEService(IEDriverManager().install()))
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-    # return driver
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.maximize_window()
+        print("Launching chrome browser.........")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        driver.maximize_window()
+        print("Launching firefox browser.........")
+    elif browser == 'Ie':
+        driver = webdriver.Ie(service=IEService(IEDriverManager().install()))
+        driver.maximize_window()
+        print("Launching Ie browser.........")
+    else:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.maximize_window()
     yield driver
     driver.quit()
 
 
-# import pytest
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-#
-# @pytest.fixture()
-# def setup():
-#     options = webdriver.ChromeOptions()
-#     options.add_argument("--headless")
-#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-#
-#     # driver = webdriver.Chrome(ChromeDriverManager().install())
-#     yield driver
-#     driver.quit()
+def pytest_addoption(parser):  # This will get the value from CLI /hooks
+    parser.addoption("--browser")
 
-# import pytest
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-#
-# @pytest.fixture()
-# def setup():
-#     # Use webdriver_manager to manage ChromeDriver
-#     chrome_version = ChromeDriverManager().get_version()
-#     driver_path = ChromeDriverManager(chrome_version=chrome_version).install()
-#
-#     # Set up ChromeOptions if needed
-#     chrome_options = webdriver.ChromeOptions()
-#     # Add options if necessary, e.g., headless mode: chrome_options.add_argument("--headless")
-#
-#     driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
-#
-#     yield driver
-#     driver.quit()
+
+@pytest.fixture()
+def browser(request):  # This will return the Browser value to method
+    return request.config.getoption("--browser")
