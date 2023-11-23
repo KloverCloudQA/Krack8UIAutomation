2bc3ca8805f58d7af01e824a6e3cd44db4dc1787
MdSikder <mdporagbsmrstu@gmail.com>
2023-11-23 15:48:26 +0600

    few updates

--- pageObjects/pom_application.py
+++ pageObjects/pom_application.py
@@ -158,3 +158,53 @@ class Application:
             print("TimeoutException error", e)
         except InvalidSessionIdException as e:
             print("InvalidSessionIdException error", e)
+
+    # -----------------------------------------Delete Application---------------------------------------------------
+
+    def go_applicationList_page(self):
+        self.logger.info("****************** go to application list page ****************")
+        self.driver.get(self.baseURL + "applications")
+        time.sleep(5)
+
+    def click_application_from_list(self):
+        self.logger.info("****************** click on application ****************")
+        self.driver.find_element(By.XPATH, "//h3[normalize-space()='" + self.application_name + "']").click()
+        time.sleep(5)
+
+    def click_on_settings(self):
+        self.logger.info("****************** click on settings ****************")
+        self.driver.find_element(By.XPATH, Locator.Namespace_settings).click()
+        time.sleep(5)
+
+    def click_on_delete(self):
+        self.logger.info("****************** click on delete ****************")
+        self.driver.find_element(By.XPATH, Locator.deleteButton_namespace).click()
+        time.sleep(5)
+
+    def provide_application_name(self):
+        self.logger.info("****************** input application name ****************")
+        self.driver.find_element(By.XPATH, Locator.Application_namebox_D).send_keys(self.namespace_name)
+        time.sleep(5)
+
+    def click_on_Delete_permanently_button(self):
+        self.logger.info("****************** click on submit button ****************")
+        self.driver.find_element(By.XPATH, Locator.Delete_permanently_button).click()
+        time.sleep(10)
+
+    def validate_namespace_deletion(self):
+        self.logger.info("****************** validate application deletion ****************")
+
+        try:
+            self.driver.refresh()
+            WebDriverWait(self.driver, 15)
+            application = self.driver.find_element(By.XPATH, "//span[normalize-space()='" + self.application_name + "']")
+
+            if not application.is_displayed():
+                print("Namespace '" + self.application_name + "' is not found")
+                assert True
+            else:
+                print("Namespace '" + self.application_name + "' is found")
+                assert False
+
+        except NoSuchElementException:
+            print("The" + self.application_name + "is not found in the list.")
